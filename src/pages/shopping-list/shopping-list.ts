98ff23836218d124d924ee7854bf64a0d293bfda
WillyLu326 <luzh0326@gmail.com>
2017-08-01 23:06:11 -0400

    two way binding & form submit to get the values

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NgForm } from '@angular/forms';
 
 /**
  * Generated class for the ShoppingListPage page.
@@ -20,6 +21,10 @@ export class ShoppingListPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ShoppingListPage');
+  }
+
+  onAddItem(form: NgForm) {
+    console.log(form);
   }
 
 }
