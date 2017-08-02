8194646f214a97ff07598db299463df9a9b2cbde
WillyLu326 <luzh0326@gmail.com>
2017-08-01 23:22:48 -0400

    create CRUD functions

--- src/services/shopping-list.service.ts
+++ src/services/shopping-list.service.ts
@@ -1,6 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Ingredient } from '../model/ingredient';
 
 @Injectable()
 export class ShoppingListService {
+
+    addOneIngredient(): Ingredient[] {
+
+    }
+
+    addIngredients(): Ingredient[] {
+
+    }
+
+    editIngredient(name: string): Ingredient {
+
+    }
+
+    removeIngredient(name: string): Ingredient[] {
+
+    }
+
+    getAllIngredients(): Ingredient[] {
+
+    }
+
+    getIngredient(name: string): Ingredient {
+        
+    }
 
 }
