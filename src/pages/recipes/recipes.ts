ea7d0809799b83273eba2295340c094d7a4cce0f
WillyLu326 <luzh0326@gmail.com>
2017-08-05 00:39:50 -0400

    jump from recipes page to edit-recipe page

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the RecipesPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import { EditRecipePage } from '../edit-recipe/edit-recipe';
 
 @IonicPage()
 @Component({
@@ -20,6 +15,10 @@ export class RecipesPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RecipesPage');
+  }
+
+  onCreateRecipe() {
+    this.navCtrl.push(EditRecipePage);
   }
 
 }
